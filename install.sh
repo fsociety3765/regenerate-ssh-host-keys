0c62f153d047826d8ad490e0829dd52433191bbe
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2022-01-01 00:43:37 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo wget https://github.com/fsociety3765/regenerate-ssh-host-keys/blob/master/regenerate-ssh-host-keys.service -P /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/regenerate-ssh-host-keys.service
