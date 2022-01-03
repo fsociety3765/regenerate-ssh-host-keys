6310e20c9d964b1e2614d3b94215a16a14fcadf0
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2022-01-03 10:11:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 sudo wget https://github.com/fsociety3765/regenerate-ssh-host-keys/blob/master/regenerate-ssh-host-keys.service -P /etc/systemd/system/
 sudo chown root:root /etc/systemd/system/regenerate-ssh-host-keys.service
+sudo systemctl daemon-reload
